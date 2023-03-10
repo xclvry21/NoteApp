698aa868c70d1c7b56d51a0e2cc9886d2fce9acd
xcl21 <axcle.viray21@gmail.com>
2023-03-10 18:06:49 +0800

    create note table and add fillable on noteModel

--- database/migrations/2023_03_10_174109_create_notes_table.php
+++ database/migrations/2023_03_10_174109_create_notes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
